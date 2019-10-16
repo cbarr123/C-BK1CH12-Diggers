405ff629fd804a8e59286bb6ffe0119fe103db2f
Carl Barringer <cbarringer@Carls-MacBook-Pro.local>
2019-10-16 10:02:22 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace diggers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
